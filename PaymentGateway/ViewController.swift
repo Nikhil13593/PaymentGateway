42a6ac9b72e7e231d89c2f28f77698103c077bc8
Nikhil Patil <npatil972@gmail.com>
2019-04-03 21:49:25 +0530

    Payment Gateway
    Using RazorPay payment gateway,try to implement payment gateway in application

--- PaymentGateway/ViewController.swift
+++ PaymentGateway/ViewController.swift
@@ -7,14 +7,90 @@
 //
 
 import UIKit
+import Razorpay
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, RazorpayPaymentCompletionProtocol {
+  
 
+    var razorpay: Razorpay!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        razorpay = Razorpay.initWithKey("RazorPay Key", andDelegate: self)
+        
     }
 
-
+    
+    @IBAction func payNowAction(_ sender: Any) {
+        
+        let options: [String:Any] = [
+            "amount" : "7800000",
+            "description": "Buy iPhone XR",
+            "image": "http://www.brninfotech.com/pulse/assets/admin/layout/img/logo-big.png",
+            "name": "Flipkart.com",
+            "prefill": [
+            "contact": "",
+            "email": ""
+            ],
+            "theme": [
+                "color": "#1478F6"
+            ]
+        ]
+        
+        razorpay.open(options)
+        
+    }
+    
+    func onPaymentError(_ code: Int32, description str: String) {
+        
+        let alertController = UIAlertController(title: "Failure", message:str, preferredStyle: UIAlertController.Style.alert)
+        
+        let cancelAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+        print(str)
+    }
+    
+    func onPaymentSuccess(_ payment_id: String) {
+        
+        let alertController = UIAlertController(title: "Success", message:"Payment Id \(payment_id)", preferredStyle: UIAlertController.Style.alert)
+        
+        let cancelAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+        
+        
+        print(payment_id)
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
